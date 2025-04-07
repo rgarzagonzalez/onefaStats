444771c612629a2789ca468f79cce2665168cf0c
Ricardo Garza <ricardo@madhouse.com.mx>
2025-04-07 16:33:06 -0600

    port binding

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hola Mundo');
 });
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
+app.listen(port, () => {
+    console.log('Servidor corriendo en el puerto ${port}');
 })
